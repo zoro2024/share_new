4be4361b984fa03c5e8f4aff7833306ffc6bb545
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-13 04:10:01 +0530

    Update commonGeneric.groovy

--- vars/commonGeneric.groovy
+++ vars/commonGeneric.groovy
@@ -1,6 +1,13 @@
 import org.generic.File1
+import org.generic.File2
 
 def call() {
     def message = File1.getFile1Message()
     echo message
 }
+
+def call() {
+    def message = File2.getFile2Message()
+    echo message
+}
+
