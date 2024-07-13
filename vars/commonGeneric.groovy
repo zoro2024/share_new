2de357277100b1333be62a7bf82ae6c47f77c0a3
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-13 16:40:32 +0530

    Update commonGeneric.groovy

--- vars/commonGeneric.groovy
+++ vars/commonGeneric.groovy
@@ -2,14 +2,26 @@ import org.generic.Checkout
 import org.generic.Gitleaks
 import org.generic.TrivyInstaller
 
-def callCheckout(url, branch, creds) {
-    new Checkout().call(url, creds, branch)
-}
-
-def callGitleaks() {
-    new Gitleaks().call()
-}
-
-def callTrivyInstaller(repoUrl) {
-    new TrivyInstaller().call(repoUrl)
+def call(String repoUrl, String credsId, String branch) {
+    node {
+        try {
+            stage('Checkout') {
+                Checkout checkout = new Checkout()
+                checkout.clone(repoUrl, credsId, branch)
+            }
+            
+            stage('Run Gitleaks') {
+                Gitleaks gitleaks = new Gitleaks()
+                gitleaks.run()
+            }
+            
+            stage('Install Trivy and Scan') {
+                TrivyInstaller trivyInstaller = new TrivyInstaller()
+                trivyInstaller.installTrivy(repoUrl)
+            }
+        } catch (Exception e) {
+            currentBuild.result = 'FAILURE'
+            throw e
+        }
+    }
 }
