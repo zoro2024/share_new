ffa34e10f4f4bf28fa8d08e0ab8e06de0d22a15c
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-13 17:45:23 +0530

    Update CommonGeneric.groovy

--- vars/CommonGeneric.groovy
+++ vars/CommonGeneric.groovy
@@ -2,38 +2,18 @@ import org.generic.Checkout
 import org.generic.Gitleaks
 import org.generic.TrivyInstaller
 
-def call(String repoUrl, String credsId, String branch) {
+def checkoutRepo(String url, String creds, String branch) {
     Checkout checkout = new Checkout()
-    Gitleaks gitleaks = new Gitleaks()
-    TrivyInstaller trivyInstaller = new TrivyInstaller()
-
-    pipeline {
-        agent any
-        
-        stages {
-            stage('Checkout') {
-                steps {
-                    script {
-                        checkout.clone(repoUrl, credsId, branch)
-                    }
-                }
-            }
-            
-            stage('Run Gitleaks') {
-                steps {
-                    script {
-                        gitleaks.run()
-                    }
-                }
-            }
-            
-            stage('Install Trivy and Scan') {
-                steps {
-                    script {
-                        trivyInstaller.installTrivy(repoUrl)
-                    }
-                }
-            }
-        }
-    }
+    checkout(url, creds, branch)
 }
+
+def runGitleaks() {
+    Gitleaks gitleaks = new Gitleaks()
+    gitleaks()
+}
+
+def installAndRunTrivy(String repoUrl = '') {
+    TrivyInstaller trivy = new TrivyInstaller()
+    trivy.installTrivy(repoUrl)
+}
+
