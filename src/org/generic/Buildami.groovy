bbd8511d48e6684c736098f0964caf5391af79c9
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-15 23:59:10 +0530

    Update Buildami.groovy

--- src/org/generic/Buildami.groovy
+++ src/org/generic/Buildami.groovy
@@ -1,18 +1,20 @@
 package org.generic
 
-class BuildAmi {
-    static def build(pipelineContext) {
+class Buildami {
+    static def build(pipelineContext, envVars, params) {
         pipelineContext.pipeline {
             agent any
 
             parameters {
-                string(name: 'AMI_NAME', defaultValue: 'packer-template', description: 'Name of the AMI to be created')
+                params.each { key, value ->
+                    string(name: key, defaultValue: value, description: "Parameter: ${key}")
+                }
             }
 
             environment {
-                PACKER_TEMPLATE = 'packer.pkr.hcl'
-                AWS_ACCESS_KEY_ID = pipelineContext.credentials('aws-access-key-id') // Assuming you have set these credentials in Jenkins
-                AWS_SECRET_ACCESS_KEY = pipelineContext.credentials('aws-secret-access-key') // Assuming you have set these credentials in Jenkins
+                envVars.each { key, value ->
+                    environmentVariables[key] = value
+                }
             }
 
             stages {
