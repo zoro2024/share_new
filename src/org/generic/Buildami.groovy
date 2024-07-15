183f1d7fd1764192dbcfba2aba07383cceba1e99
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-16 00:12:37 +0530

    Update Buildami.groovy

--- src/org/generic/Buildami.groovy
+++ src/org/generic/Buildami.groovy
@@ -1,34 +1,8 @@
 package org.generic
 
-class Buildami {
-    static def build(pipelineContext, envVars, params) {
-        pipelineContext.pipeline {
-            agent any
-
-            parameters {
-                params.each { key, value ->
-                    string(name: key, defaultValue: value, description: "Parameter: ${key}")
-                }
-            }
-
-            environment {
-                envVars.each { key, value ->
-                    environmentVariables[key] = value
-                }
-            }
-
-            stages {
-                stage('Build AMI') {
-                    steps {
-                        script {
-                            sh """
-                            packer init ${env.PACKER_TEMPLATE}
-                            packer build -var 'ami_name=${params.AMI_NAME}' ${env.PACKER_TEMPLATE}
-                            """
-                        }
-                    }
-                }
-            }
-        }
-    }
+def call(packerTemplate, amiName) {
+    sh """
+    packer init ${packerTemplate}
+    packer build -var 'ami_name=${amiName}' ${packerTemplate}
+    """
 }
