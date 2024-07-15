f8c2d41a3964f5f30065fb800f864c60dd111d70
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-15 23:49:53 +0530

    Update CommonGeneric.groovy

--- vars/CommonGeneric.groovy
+++ vars/CommonGeneric.groovy
@@ -1,17 +1,21 @@
-// shared-new/vars/CommonGeneric.groovy
+// import org.generic.Checkout
+// import org.generic.Gitleaks
+// import org.generic.TrivyInstaller
 
-import org.generic.Checkout
-import org.generic.Gitleaks
-import org.generic.TrivyInstaller
+// def checkoutRepo(String url, String creds, String branch) {
+//     new Checkout().call(url, creds, branch)
+// }
 
-def checkoutRepo(String url, String creds, String branch) {
-    new Checkout().call(url, creds, branch)
-}
+// def runGitleaks() {
+//     new Gitleaks().call()
+// }
 
-def runGitleaks() {
-    new Gitleaks().call()
-}
+// def installAndRunTrivy(String repoUrl = '') {
+//     new TrivyInstaller().installTrivy(repoUrl)
+// }
 
-def installAndRunTrivy(String repoUrl = '') {
-    new TrivyInstaller().installTrivy(repoUrl)
+import org.generic.Buildami
+
+def buildami() {
+    Buildami.build(this)
 }
